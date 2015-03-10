2deb5cd921720d05e5cce9a3e17f134dc04909d9
Hildequias Júnior <juniorwelling@yahoo.com.br>
2015-03-10 14:14:58 -0300

    Update config.php

--- server/config.php
+++ server/config.php
@@ -2,8 +2,8 @@
 
 //DataBase Config
 
-define("DB_HOST","pixonsoft.com");
-define("DB_NAME","pixonsof_logistic");
-define("DB_USER","pixonsof_sa");
-define("DB_PASSWORD","!Jr14061886");
+define("DB_HOST","localhost");
+define("DB_NAME","logistic");
+define("DB_USER","root");
+define("DB_PASSWORD","");
 define( 'BASEPATH', getcwd() . DIRECTORY_SEPARATOR );
